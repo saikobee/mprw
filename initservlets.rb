885b70383285e412f0004d01801b4f98cce8077f
Brian Mock <mock.brian@gmail.com>
2010-12-28 22:35:07 -0800

    Added more messaging functionality

--- initservlets.rb
+++ initservlets.rb
@@ -1,17 +1,25 @@
 class InitServlets
+    EOL = "\r\n"
+
     def initialize server, mpd
         @server = server
         @mpd    = mpd
 
+        # Percentage by which to increment volume
+        @d_volume = 5
+
         # Maps "directories" on the server to methods in this class
         @mapping = {
-            "prev"      => :prev,
-            "next"      => :next,
-            "stop"      => :stop,
-            "pause"     => :pause,
-            "play"      => :play,
-            "toggle"    => :toggle,
-            "debug"     => :debug,
+            "prev"          => :prev,
+            "next"          => :next,
+            "stop"          => :stop,
+            "pause"         => :pause,
+            "play"          => :play,
+            "playpause"     => :play_pause,
+            "debug"         => :debug,
+            "volup"         => :vol_up,
+            "voldown"       => :vol_down,
+            "nowplaying"    => :now_playing,
         }
 
         # Prefix to the "directories" above
@@ -52,9 +60,31 @@ class InitServlets
         resp.body = "Play"
     end
 
-    def toggle req, resp
-        @mpd.pause = ! @mpd.paused?
-        resp.body  = "Toggle"
+    def play_pause req, resp
+        @mpd.pause = !  @mpd.paused?
+        resp.body  = if @mpd.paused?
+            then "Pause"
+            else "Play"
+        end
+    end
+
+    def vol_up req, resp
+        @mpd.volume += @d_volume
+        resp.body = "Volume #{@mpd.volume}"
+    end
+
+    def vol_down req, resp
+        @mpd.volume -= @d_volume
+        resp.body = "Volume #{@mpd.volume}"
+    end
+
+    def now_playing req, resp
+        song = @mpd.current_song
+
+        stats = %w[title album artist time]
+        stats.each do |stat|
+            resp.body << stat << " " << song.send(stat) << EOL
+        end
     end
 
     def debug req, resp
